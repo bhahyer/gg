3d81e3f6e83b92ff77186c040c14b8b1fba06aa1
bhahyer <trustbhai5@gmail.com>
2025-05-13 13:09:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY DARINDAxOWNER
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7960283920:AAHul_6tNoU3sCL2taqTrdOYWTNOpxJu1wY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@DARINDAxOWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1944182800")
+
+
+
+
+
+
+
+  
